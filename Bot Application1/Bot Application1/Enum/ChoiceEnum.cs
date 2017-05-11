0a858f97ebf5a05bf686d8d67f6561645d632eaa
clopez <cedrik.lopez@gfi.fr>
2017-05-11 16:36:34 +0200

    ajout classe enum

---
+++ Bot Application1/Bot Application1/Enum/ChoiceEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1.Enum
+{
+    public enum ChoiceEnum
+    {
+        Horaires,
+        Contact,
+        Cra,
+        Absences,
+        Conges
+
+    }
+}
